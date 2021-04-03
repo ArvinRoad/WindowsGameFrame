8c098aff47f261cba9ac8623361c78709b7a0d2e
ArvinRoad <53329456+ArvinRoad@users.noreply.github.com>
2021-04-03 14:49:50 +0800

    GameDemo创建

---
+++ GameDemo/empty.c
@@ -0,0 +1,31 @@
+#include "interface.h"
+
+//初始化游戏
+int initGame() {
+	return 0;
+}
+
+//反初始化
+void uninitGame() {
+
+}
+
+//资源加载
+int loadGameResource() {
+	return 0;
+}
+
+//事件响应
+void processGameEvent() {
+
+}
+
+//游戏更新函数(写入)
+int updateGame() {
+	return 0;
+}
+
+//游戏渲染函数(只读)
+void renderGame() {
+
+}
